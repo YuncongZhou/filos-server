470de0a18636a3232c7e58bb0c636969a2c28603
Alex Zhou <azhou@envio360.com>
2018-08-08 22:53:02 -0400

    apollo demo

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { ApolloServer, gql } from 'apollo-server'
+const server = new ApolloServer({  });
+
+// const books:Array<> = [
+//   {
+//     title: 'Harry Potter and the Chamber of Secrets',
+//     author: 'J.K. Rowling',
+//   },
+//   {
+//     title: 'Jurassic Park',
+//     author: 'Michael Crichton',
+//   },
+// ];
